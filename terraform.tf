46e3a6361af234ba1d81e5f1608f710577ce93f1
Chenikala9 <113289421+Chenikala9@users.noreply.github.com>
2023-04-19 11:29:15 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -4,7 +4,7 @@
       secret_key = " PlHncwNjZi2SCSIuaef7SWhEVJoMhxdPl8tOVg1m "
  }
  resource "aws_instance" "MyAWSVM" {
-      ami= " ami-0ae0c7f8b67be1ffe " 
+      ami= "ami-0f960c8194f5d8df5 " 
       instance_type= "t2.micro"
       key_name= " ansible server "
       security_groups = [" gova "]
