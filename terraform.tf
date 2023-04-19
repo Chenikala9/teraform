885e2b65cf8623498aea4159fead40f8d97d05dd
Chenikala9 <113289421+Chenikala9@users.noreply.github.com>
2023-04-19 10:26:54 +0530

    update

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
  provider "aws" {
-     region = "ap-south-1"
+     region = "eu-north-1"
       access_key = "AKIAZW4BPOGJZNZVIF35 "
       secret_key = " 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 "
  }
